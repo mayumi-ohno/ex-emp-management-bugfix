48cca63ba14c02c0c4e28abfedb65d80a83168a5
mayumi-ohno <mayumi.ono@rakus-partners.co.jp>
2020-02-20 17:05:03 +0900

    springsecurity導入

--- src/main/java/jp/co/sample/emp_management/config/AdministratorsConfig.java
+++ src/main/java/jp/co/sample/emp_management/config/AdministratorsConfig.java
@@ -1,18 +1,31 @@
 package jp.co.sample.emp_management.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-public class AdministratorsConfig extends WebSecurityConfigurerAdapter{
-	
-	@Override
-	protected void configure(HttpSecurity web) throws Exception{
-		
-		web.formLogin().loginPage("/").defaultSuccessUrl("list").failureUrl("505").permitAll();
-//		web.authorizeRequests().antMatchers("/css/**", "/images/**", "/js/**").permitAll().anyRequest().authenticated();
-	}
-	
+@Configuration
+class DAdministratorsConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				//ログイン前にアクセス可とするファイル群
+				.mvcMatchers("/").permitAll()
+				.mvcMatchers("/css/style.css").permitAll()
+				.mvcMatchers("/css/bootstrap.css").permitAll()
+				.mvcMatchers("/img/e1.png").permitAll()
+				.mvcMatchers("/img/e2.png").permitAll()
+				.mvcMatchers("/img/header_logo_small.png").permitAll()
+				.mvcMatchers("/img/header_logo.png").permitAll()
+				// 上記以外のファイルは、ログイン以前のアクセス不可とする
+				.anyRequest().authenticated().and()
+				// LOGIN
+				.formLogin()
+				.loginPage("/employee/showList")
+				.defaultSuccessUrl("/employee/showList")
+				.failureUrl("/404")
+		// end
+		;
+	}
 }
